68f584fdb92f1fc4862c244a9978c542c7342470
Aaron Chevalier <atgc@bu.edu>
2020-10-04 01:48:42 -0400

    updated tests

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,8 +1,8 @@
 context("Test Plotting Functions")
-library("BAGEL")
+library("musicatk")
 
 test_that(desc = "Testing Exposures Plotting", {
-  result <- readRDS(system.file("testdata", "res.rds", package = "BAGEL"))
+  result <- readRDS(system.file("testdata", "res.rds", package = "musicatk"))
   p <- plot_exposures(result)
   expect_true(ggplot2::is.ggplot(p))
 })
