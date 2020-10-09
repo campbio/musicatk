691dc2aaeea0a0b404eeb750a425ae506af9dda0
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2020-10-08 21:56:08 -0400

    Refactor of the refactor. plot_exposures now back to a single function for bar and box plots

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -3,6 +3,10 @@ library("musicatk")
 
 test_that(desc = "Testing Exposures Plotting", {
   result <- readRDS(system.file("testdata", "res.rds", package = "musicatk"))
-  p <- plot_exposure_bar(result)
+  p <- plot_exposures(result, plot_type = "bar")
+  expect_true(ggplot2::is.ggplot(p))
+  p <- plot_exposures(result, plot_type = "violin")
+  expect_true(ggplot2::is.ggplot(p))
+  p <- plot_exposures(result, plot_type = "box")
   expect_true(ggplot2::is.ggplot(p))
 })
