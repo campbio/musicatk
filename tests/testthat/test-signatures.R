38324f8b3eb98513444651e8ba66676da67565eb
achevali <atgc@bu.edu>
2020-10-15 16:36:04 -0400

    Bio review (#5)
    * address all bioc requirements, move with_seed from code to examples, move data to /inst/extdata and /data

--- tests/testthat/test-signatures.R
+++ tests/testthat/test-signatures.R
@@ -2,16 +2,9 @@ context("Test Find Signature Functions")
 library("musicatk")
 
 test_that(desc = "Inputs are correct", {
-  #incomplete_musicatk <- readRDS(system.file("testdata", "musicatk",
-  #                        package = "musicatk"))
-
-  #expect_error(discover_signatures(incomplete_musicatk, "SBS96"),
-  #             regexp = "malformed")
-  #expect_error(predict_exposure(incomplete_musicatk, "SNV96", cosmic_v2_sigs),
-  #             regexp = "malformed")
-
   expect_error(discover_signatures(data.frame(0)), regexp = "must be a")
-  #expect_error(predict_exposure(methods::new("bagel", variants =
-  #                                             data.table::data.table(0)),
-  #                              "SNV96", cosmic_v2_sigs), regexp = "malformed")
+  g <- select_genome("19")
+  expect_error(predict_exposure(methods::new("musica", variants =
+                                               data.table::data.table(0)), g,
+                                "SBS96", cosmic_v2_sigs), regexp = "malformed")
 })
