a1e097f3394834d431b7f52d3bbbbcf5db647382
nathansahelijo <sahelijo@bu.edu>
2020-11-24 23:01:51 -0500

    update function after merge with upstream

--- R/differential_analysis.R
+++ R/differential_analysis.R
@@ -1,19 +1,22 @@
-#' Compare exposures from annotated samples
-#' 
 #' @importFrom magrittr %>%
 #' @importFrom dplyr mutate
 #' @importFrom MASS glm.nb
+
+#' @title Compare exposures of annotated samples
+#' @description \code{compare_samples} is used to run differential analysis on 
+#' the signature exposures of annotated samples within the 
+#' \code{\linkS4class{musica_result}} object.
 #' @param musica_result A \code{\linkS4class{musica_result}} object 
 #' @param annotation Column in the sample_annotations table of the 
 #' \code{\linkS4class{musica_result}} object
-#' @param method Any method in c("wilcox", "kruskal", "glm.nb") used to perform differential analysis on signature 
-#' exposures
+#' @param method Any method in \code{c("wilcox", "kruskal", "glm.nb")} 
+#' used to perform differential analysis on signature exposures
 #' @param ... Additional arguments to be passed to the chosen method
 #' @return A matrix containing statistics summarizing the analysis dependent
 #' on the chosen method
 #' @examples 
-#' musica <- readRDS(system.file("testdata", "res_annot.rds", package = "musicatk"))
-#' compare_samples(musica, "Tumor_Subtypes", method="wilcox")
+#' data("res_annot")
+#' compare_samples(res_annot, "Tumor_Subtypes", method="wilcox")
 #' @export
 compare_samples <- function(musica_result, annotation, method="wilcox",...) {
   if (!methods::is(musica_result, "musica_result")) {
@@ -24,14 +27,15 @@ compare_samples <- function(musica_result, annotation, method="wilcox",...) {
   if (is.null(annotations)) {
     stop(paste('"',annotation,'" does not exist in musica_result.', sep=""))
   }
+  exposures <- exposures(musica_result)
   groups <- unique(annotations)
   annotations <- 
     annotations[match(musica_result@musica@sample_annotations$Samples,
-                      colnames(musica_result@exposures))]
+                      colnames(exposures))]
   annotations <- factor(annotations, levels=unique(groups))
   diff.out <- 0
-  exposures <- musica_result@exposures
   l <- length(exposures)
+  browser()
   if (method=="wilcox" || is.null(method)) {
     pairs <- combn(groups,2) %>% t()
     header <- data.frame(y=pairs[,1], x=pairs[,2]) %>%
@@ -89,3 +93,28 @@ compare_samples <- function(musica_result, annotation, method="wilcox",...) {
   return (diff.out)
 }
 
+# lusc_maf <- system.file("extdata", "public_TCGA.LUSC.maf", package = "musicatk")
+# lusc.variants <- extract_variants_from_maf_file(maf_file=lusc_maf) %>% 
+#   mutate(.keep="all", Tumor_Type="meso")
+# luad_vcf <- system.file("extdata", "public_LUAD_TCGA-97-7938.vcf", package = "musicatk") 
+# luad.variants <- extract_variants_from_vcf_file(vcf_file = luad_vcf) %>% 
+#   mutate(.keep="all", Tumor_Type="thca")
+# 
+# dmy1 <- lusc.variants %>% mutate(Tumor_Type="lusc_A",
+#                             sample=paste("lusc_A-", sample, sep=""))
+# dmy2 <- lusc.variants %>% mutate(Tumor_Type="lusc_B",
+#                             sample=paste("lusc_B-", sample, sep=""))
+# 
+# mix.variants <- rbind(dmy1, luad.variants, dmy2)
+# mix.musica <- create_musica(x=mix.variants, genome=g)
+# 
+# build_standard_table(mix.musica, g = g, table_name = "SBS96")
+# samp_annot(mix.musica)
+# samp_annot(mix.musica, "Tumor_Type") <- 
+#   c("lusc_A", "lusc_A", "lusc_A", "lusc_B", "lusc_B", "lusc_B","luad")
+# 
+# res_diff <- discover_signatures(musica = mix.musica,
+#                                   table_name = "SBS96",
+#                                   num_signatures = 7,
+#                                   method = "lda")
+# compare_samples(res_diff, "Tumor_Type")
--- tests/testthat/test-differentialanalysis.R
+++ tests/testthat/test-differentialanalysis.R
@@ -3,22 +3,21 @@ library("musicatk")
 
 test_that(desc = "Diff Anal Input validity", {
   expect_error(compare_samples(data.frame(0)), regexp = "must be a")
-  result <- readRDS(system.file("testdata", "res_annot.rds", package = "musicatk"))
-  expect_error(compare_samples(result, "dummy", method="wilcox"), 
+  data("res_annot")
+  expect_error(compare_samples(res_annot, "dummy", method="wilcox"), 
                regexp="does not exist")
-  expect_error(compare_samples(result, "Tumor_Subtypes", method="dummy"), 
+  expect_error(compare_samples(res_annot, "Tumor_Subtypes", method="dummy"), 
                regexp="Method is not supported")
 })
 
 test_that(desc = "Diff Anal functionality",  {
-  result <- readRDS(
-    system.file("testdata", "res_annot.rds", package = "musicatk"))
-  expect_type(compare_samples(result, "Tumor_Subtypes", method="wilcox"), 
+  data("res_annot")
+  expect_type(compare_samples(res_annot, "Tumor_Subtypes", method="wilcox"), 
                   "double")
-  expect_type(compare_samples(result, "Tumor_Subtypes", method="kruskal"), 
+  expect_type(compare_samples(res_annot, "Tumor_Subtypes", method="kruskal"), 
               "double")
-  expect_warning(compare_samples(result, "Tumor_Subtypes", method="glm.nb"), 
+  expect_warning(compare_samples(res_annot, "Tumor_Subtypes", method="glm.nb"), 
                  regexp="NaNs produced")
-  suppressWarnings(expect_type(compare_samples(result, "Tumor_Subtypes", method="glm.nb"), 
+  suppressWarnings(expect_type(compare_samples(res_annot, "Tumor_Subtypes", method="glm.nb"), 
               "double"))
 })
