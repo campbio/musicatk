d57df30148663b471c8cde6c63c0805358afdd9e
zeenu94 <56841101+zeenu94@users.noreply.github.com>
2020-12-16 09:42:31 -0500

    Heatmap (#16)
    * Added heatmap plotting

---
+++ tests/testthat/test-heatmap.R
@@ -0,0 +1,19 @@
+context("Test Heatmap plotting function")
+library("musicatk")
+
+test_that(desc = "Testing Exposures Heatmap Plotting", {
+  data(res_annot)
+  p <- plot_heatmap(res_annot)
+  expect_true(!is.null(p))
+  p <- plot_heatmap(res_annot, proportional = TRUE)
+  expect_true(!is.null(p))
+  p <- plot_heatmap(res_annot, proportional = TRUE,scale = TRUE)
+  expect_true(!is.null(p))
+  p <- plot_heatmap(res_annot, show_column_names = TRUE)
+  expect_true(!is.null(p))
+  p <- plot_heatmap(res_annot, show_row_names = TRUE)
+  expect_true(!is.null(p))
+  p <- plot_heatmap(res_annot, show_column_names = TRUE, show_row_names = TRUE)
+  expect_true(!is.null(p))
+  
+})
