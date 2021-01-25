05be7f9cb9479d8ddd4ab1c154b27355d81fa122
nathansahelijo <sahelijo@bu.edu>
2021-01-24 19:51:07 -0500

    remove import from ui.R

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -16,36 +16,12 @@ ui <- fluidPage(
       menuItem("Help", tabName = "widgets", icon = icon("th")))),
     
     dashboardBody(
-      tabItems(
-        tabItem(
-          tabName = "import",
-          h2("Import Data "),
-          box(
-            radioButtons("data", "Select Data:",
-                         c("MAF file" = "maf",
-                           "VCF file" = "vcf",
-                           "Example Data" = "exp")),
-            actionButton("get_musica", "Get Musica Data"),
-            actionButton("get_musica_result","Get Musica Result Object"),
-            fileInput("file_vcf", "VCF File",
-                      multiple = TRUE,
-                      accept = ".vcf"),
-            fileInput("file_maf", "MAF File",
-                      multiple = TRUE,
-                      accept = ".maf"),
-            mainPanel(
-              
-              # Output: Data file ----
-              tableOutput("contents")
-              
-            )
-          
-          
-        )),
+        tabItems(
+          tabItem(tabName = "import"),
         
 ###################### Nathan's Code ##########################################
-        tabItem(tabName = "tables", h2("Create Tables"), shinyPanelTables),
-        tabItem(tabName = "signatures", h2("Signatures and Exposures"), 
+          tabItem(tabName = "tables", h2("Create Tables"), shinyPanelTables),
+          tabItem(tabName = "signatures", h2("Signatures and Exposures"), 
                 shinyPanelDiscover)
       )
 ###############################################################################
