a435e99877b8d49bf2f061460ec343d9a24f0edc
Zainab Khurshid <zeenu@Zainabs-MacBook-Pro.local>
2021-02-04 13:38:07 -0500

    shiny-commit-import

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,5 +1,7 @@
 library(shinydashboard)
-
+source("ui_import.R",local = T)
+source("ui_genome.R", local = T)
+source("ui_musica.R",local = T)
 source("ui_discover.R", local = T)
 source("ui_tables.R", local = T)
 
@@ -10,6 +12,8 @@ ui <- fluidPage(
     dashboardHeader(title = "Musicatk"),
     dashboardSidebar(sidebarMenu(
       menuItem("Import", tabName = "import", icon = icon("th")),
+      menuItem("Genome", tabName = "genome", icon = icon("th")),
+      menuItem("Musica", tabName = "musica", icon = icon("th")),
       menuItem("Tables", tabName = "tables", icon = icon("th")),
       menuItem("Signatures", tabName = "signatures", icon = icon("th")),
       menuItem("Data Visualization", tabName = "visualization", icon = icon("th")),
@@ -17,7 +21,9 @@ ui <- fluidPage(
     
     dashboardBody(
         tabItems(
-          tabItem(tabName = "import"),
+          tabItem(tabName = "import", h2("Import Data", shinyPanelImport)),
+          tabItem(tabName = "genome",  shinyPanelGenome),
+          tabItem(tabName = "musica", shinyPanelMusica),
         
 ###################### Nathan's Code ##########################################
           tabItem(tabName = "tables", h2("Create Tables"), shinyPanelTables),
