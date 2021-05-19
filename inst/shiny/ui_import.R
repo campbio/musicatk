9c972f298709a1c245caf0fd3b1ec9041b14c2a6
Zainab Khurshid <zeenu@Zainabs-MacBook-Pro.local>
2021-05-18 23:47:32 -0400

    git-commit

--- inst/shiny/ui_import.R
+++ inst/shiny/ui_import.R
@@ -1,12 +1,12 @@
 shinyPanelImport <- fluidPage(
           fluidRow(
-            useShinyjs(),
-            column(width = 12,
+            
+                useShinyjs(),
                    # h3("Step 1: Select File")
             )
-          ),
+          ,
           hr(),
-          fluidRow(box(width = 12,
+          fluidRow(box(width = 6,
             useShinyalert(),
             add_busy_spinner(spin = "fading-circle"),
             tags$style("#file {
@@ -18,7 +18,7 @@ shinyPanelImport <- fluidPage(
                   accept = c(".maf",".vcf")),
           actionButton("upload", "Add samples"))),
           
-          fluidRow(box(width = 12,h3("Added files"),
+          fluidRow(box(width = 6,h3("Added files"),
           #tags$div(id = "file_id",dataTableOutput("my_file_name"),style =  "font-size:40%"),
           tags$div(id = "file_id",DT::dataTableOutput("dtable"),style = "font-size:40%; overflow-y: scroll;overflow-x: scroll;"),
           uiOutput('undoUI')),
@@ -26,8 +26,8 @@ shinyPanelImport <- fluidPage(
           
           uiOutput("spinner")),
           
-          fluidRow(box(width = 12,div(dataTableOutput("musica_contents"), style = "height:500px; overflow-y: scroll;overflow-x: scroll;"), actionButton("import", "Import"),
-                       downloadButton("download_musica", "Download Variants"))),
+          fluidRow(box(width = 6,actionButton("import", "Import"),downloadButton("download_musica", "Download Variants"),div(dataTableOutput("musica_contents"), style = "height:500px; overflow-y: scroll;overflow-x: scroll;") 
+                       )),
           bsTooltip("upload", "Press button to add your uploaded files to Sample List", placement = "bottom", trigger = "hover",
                     options = NULL),
           bsTooltip("file_id", "Table of files that have been added by you", placement = "bottom", trigger = "hover",
