6c968d568c4d5c62aca025a63787299c694a48da
BioTong <tongtong950624@gmail.com>
2021-07-06 22:52:49 -0400

    tutorial web

--- inst/shiny/ui_help.R
+++ inst/shiny/ui_help.R
@@ -1,4 +1,5 @@
 shinyPanelHelp <- fluidPage(
   #withMathJax(includeMarkdown("musicatk.Rmd"))
   #mainPanel(includeHTML("https://rdrr.io/github/campbio/musicatk/f/vignettes/musicatk.Rmd")
+  #includeHTML("C:/Users/tongt/Documents/GitHub/musicatk/inst/shiny/musicatk_help.html")
 )
